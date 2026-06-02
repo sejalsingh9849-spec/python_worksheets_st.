d4d69c281a6b8c31f7a51ecec679128e1054dd53
sejalsingh9849-spec <sejalsingh9849@gmail.com>
2026-06-02 11:55:26 +0530

    Update swapping.py

--- swap.py/swapping.py
+++ swap.py/swapping.py
@@ -1,8 +1,37 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+def swap_numbers(a, b, choice):
 
-a, b = b, a
+    if choice == 1:
+        # Using temporary variable
+        temp = a
+        a = b
+        b = temp
 
-print("After swapping:")
-print("a =", a)
-print("b =", b)
+    elif choice == 2:
+        # Using arithmetic operators
+        a = a + b
+        b = a - b
+        a = a - b
+
+    elif choice == 3:
+        # Using tuple unpacking
+        a, b = b, a
+
+    else:
+        print("Invalid Choice")
+        return
+
+    print("\nAfter Swapping:")
+    print("First Number =", a)
+    print("Second Number =", b)
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print("\nChoose Swapping Method")
+print("1. Temporary Variable")
+print("2. Arithmetic Method")
+print("3. Tuple Unpacking")
+
+choice = int(input("Enter Your Choice: "))
+
+swap_numbers(num1, num2, choice)
